716ab08d7381c1d1094f3d60bcb8213964749174
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-19 22:46:50 +0900

    他ユーザの友だちを見れないようにvalidation追加

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,15 @@
 class FriendsController < ApplicationController
   before_action :authenticate_user!
+  before_action :private_user
 
   def index
     @user = User.find(params[:user_id])
+  end
+
+  private
+  def private_user
+    if not current_user == User.find_by(id: params[:user_id])
+      redirect_to user_friends_path(:user_id => current_user.id)
+    end
   end
 end
