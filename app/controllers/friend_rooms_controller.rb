3bdd35fa6562e332756754dc3be0dd5614484096
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-19 20:33:59 +0900

    fix a little

--- app/controllers/friend_rooms_controller.rb
+++ app/controllers/friend_rooms_controller.rb
@@ -1,8 +1,9 @@
 class FriendRoomsController < ApplicationController
   before_action :authenticate_user!
   def show
-    self_id = Friend.find_by(id: params[:id]).from_user_id
-    friend_id = Friend.find_by(id: params[:id]).to_user_id
+    @friend = Friend.find_by(id: params[:id])
+    self_id = @friend.from_user_id
+    friend_id = @friend.to_user_id
     another_room_id = Friend.find_by(from_user_id: friend_id,
                                      to_user_id: self_id).id
     @self_messages = PersonalMessage.where(friend_id: params[:id])
