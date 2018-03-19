063a62c6276ab1cf2949caee9018d5dc6842ea90
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-19 20:16:52 +0900

    FriendRoomのViewを作成

--- app/controllers/friend_rooms_controller.rb
+++ app/controllers/friend_rooms_controller.rb
@@ -5,7 +5,8 @@ class FriendRoomsController < ApplicationController
     friend_id = Friend.find_by(id: params[:id]).to_user_id
     another_room_id = Friend.find_by(from_user_id: friend_id,
                                      to_user_id: self_id).id
-    @self_messages = PersonalMessage.find_by(id: params[:id])
-    @friend_messages = PersonalMessage.find_by(id: another_room_id)
+    @self_messages = PersonalMessage.where(friend_id: params[:id])
+    @friend_messages = PersonalMessage.where(friend_id: another_room_id)
+    @personal_messages = (@self_messages + @friend_messages).sort_by { |message| message.created_at }
   end
 end
