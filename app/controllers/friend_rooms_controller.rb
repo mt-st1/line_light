735d61c230a630c17f3f863df34486277532df8a
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-19 17:59:03 +0900

    friend_rooms#showメソッド実装

--- app/controllers/friend_rooms_controller.rb
+++ app/controllers/friend_rooms_controller.rb
@@ -3,5 +3,9 @@ class FriendRoomsController < ApplicationController
   def show
     self_id = Friend.find_by(id: params[:id]).from_user_id
     friend_id = Friend.find_by(id: params[:id]).to_user_id
+    another_room_id = Friend.find_by(from_user_id: friend_id,
+                                     to_user_id: self_id).id
+    @self_messages = PersonalMessage.find_by(id: params[:id])
+    @friend_messages = PersonalMessage.find_by(id: another_room_id)
   end
 end
--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -1,3 +1,4 @@
 class PersonalMessage < ApplicationRecord
   belongs_to :friend
+  validates :friend, presence: true
 end
