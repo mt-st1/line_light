08adc003641b7ea67deb6e107c6b1b9a558b1da8
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-19 22:34:43 +0900

    本人のみFriendRoom見れるようにvalidation追加

--- app/controllers/friend_rooms_controller.rb
+++ app/controllers/friend_rooms_controller.rb
@@ -1,7 +1,8 @@
 class FriendRoomsController < ApplicationController
   before_action :authenticate_user!
+  before_action :private_user
+
   def show
-    @friend = Friend.find_by(id: params[:id])
     self_id = @friend.from_user_id
     friend_id = @friend.to_user_id
     another_room_id = Friend.find_by(from_user_id: friend_id,
@@ -9,5 +10,16 @@ class FriendRoomsController < ApplicationController
     @self_messages = PersonalMessage.where(friend_id: params[:id])
     @friend_messages = PersonalMessage.where(friend_id: another_room_id)
     @personal_messages = (@self_messages + @friend_messages).sort_by { |message| message.created_at }
+  end
+
+  private
+  def private_user
+    if @friend = Friend.find_by(id: params[:id])
+      if not current_user == @friend.from_user
+        redirect_to user_friends_path(:user_id => current_user.id)
+      end
+    else
+      redirect_to user_friends_path(:user_id => current_user.id)
+    end
   end
 end
