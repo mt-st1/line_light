425bc3fe2367f0ea8c7a65c9d907bc27cff3db49
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-22 19:24:34 +0900

    production設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'vast-garden-28330' }
+  config.action_mailer.default_url_options = { :host => ENV['HEROKU_APP_DOMAIN'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
