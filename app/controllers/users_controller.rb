5c01c964473f832a47bac1cfae17e199e73f4588
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-23 21:08:01 +0900

    友達ではないユーザの情報は見れないように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_action :authenticate_user!
-before_action :admin_user, only: [:show_all]
+  before_action :authenticate_user!
+  before_action :admin_user, only: [:show_all]
+  before_action :friend_user, only: [:show]
 
   def show
     @user = User.find(params[:id])
@@ -15,5 +16,23 @@ before_action :admin_user, only: [:show_all]
     if not current_user.admin_flg?
       redirect_to user_path(:id => current_user.id), :alert => '管理者権限がありません'
     end
+  end
+
+  def friend_user
+    if target = User.find_by(id: params[:id])
+      if current_user == target
+        redirect_to home_path
+      elsif not current_user.friend_with?(target)
+        begin
+          redirect_to :back, :alert => '友達ではないユーザの情報は見れません'
+        rescue ActionController::RedirectBackError
+          redirect_to_user_friends
+        end
+      end
+    end
+  end
+
+  def redirect_to_user_friends
+    redirect_to user_friends_path(:user_id => current_user.id), :alert => '友達ではないユーザの情報は見れません'
   end
 end
