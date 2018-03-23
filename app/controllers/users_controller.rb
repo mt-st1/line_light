366a8f2159e443a98f47150813485a814b8a6a72
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-23 19:59:17 +0900

    管理者のみユーザ一覧にアクセスできるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 before_action :authenticate_user!
+before_action :admin_user, only: [:show_all]
 
   def show
     @user = User.find(params[:id])
@@ -7,5 +8,12 @@ before_action :authenticate_user!
 
   def show_all
     @users = User.all
+  end
+
+  private
+  def admin_user
+    if not current_user.admin_flg?
+      redirect_to user_path(:id => current_user.id), :alert => '管理者権限がありません'
+    end
   end
 end
