4aec8218099bf172946b3187d198eb26b18715b9
Takumi Ishiyama <mt.stone20@gmail.com>
2018-03-28 13:39:03 +0900

    FriendRoomにいるときのみfriend_room_channelをsubscribe

--- app/channels/friend_room_channel.rb
+++ app/channels/friend_room_channel.rb
@@ -1,11 +1,12 @@
 class FriendRoomChannel < ApplicationCable::Channel
   def subscribed
-    friend = Friend.find_by(id: params[:room_id])
-    relative_friend = Friend.find_by(from_user_id: friend.to_user_id, to_user_id: friend.from_user_id)
-    if current_user.id == friend.from_user_id
-      stream_from "friend_room_channel_#{params[:room_id]}"
-    elsif current_user.id == friend.to_user_id
-      stream_from "friend_room_channel_#{relative_friend.id}"
+    if friend = Friend.find_by(id: params[:room_id])
+      relative_friend = Friend.find_by(from_user_id: friend.to_user_id, to_user_id: friend.from_user_id)
+      if current_user.id == friend.from_user_id
+        stream_from "friend_room_channel_#{params[:room_id]}"
+      elsif current_user.id == friend.to_user_id
+        stream_from "friend_room_channel_#{relative_friend.id}"
+      end
     end
   end
 
