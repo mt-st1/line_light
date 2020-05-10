0f413063777276a8807b967721ea9375ec004762
Takumi Ishiyama <mt.stone20@gmail.com>
2020-05-10 14:27:06 +0900

    Fix url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,10 +36,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://vast-garden-28330.herokuapp.com/cable'
+  config.action_cable.url = 'wss://line-light-demo.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   # WebSocketの接続元の制限を緩める
-  config.action_cable.allowed_request_origins = [ 'https://vast-garden-28330.herokuapp.com', /http:\/\/vast-garden-28330.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://line-light-demo.herokuapp.com', /http:\/\/line-light-demo.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
